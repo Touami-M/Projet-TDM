2893988c87d8ff4ec88eeceec17df62907e710de
Ayoub <1234>
2023-01-05 10:42:16 +0100

    I added the slider for the brands in the home screen

---
+++ app/src/main/java/com/example/rentgo/Brand.kt
@@ -0,0 +1,8 @@
+package com.example.rentgo
+
+import java.io.Serializable
+
+class Brand (
+        var brandImage: Int = 0,
+        var brandName: String = "",
+): Serializable {}
--- app/src/main/java/com/example/rentgo/HomeFragment.kt
+++ app/src/main/java/com/example/rentgo/HomeFragment.kt
@@ -8,12 +8,15 @@ import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.rentgo.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment(){
     lateinit var binding: FragmentHomeBinding
     lateinit var carModel: CarModel
+    lateinit var brandModel: BrandModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,6 +30,13 @@ class HomeFragment : Fragment(){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        brandModel = ViewModelProvider(requireActivity()).get(BrandModel::class.java)
+        binding.brandsRecyclerView.layoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL,false)
+        binding.brandsRecyclerView.adapter = BrandAdapter(requireActivity(),brandModel.brands)
+        val itemDecor1 = DividerItemDecoration(requireActivity(),1)
+        binding.carsRecycleView.addItemDecoration(itemDecor1)
+
         carModel = ViewModelProvider(requireActivity()).get(CarModel::class.java)
         binding.carsRecycleView.layoutManager = GridLayoutManager(requireActivity(),resources.getInteger(R.integer.nbcol))
         binding.carsRecycleView.adapter = CarAdapter(requireActivity(),carModel.cars)

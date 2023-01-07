cbe4e6e22040b184b7de6d4d7cfc41eaa16c0624
Touami Mohammed <jm_touami@esi.dz>
2023-01-07 02:07:37 +0100

    Added login and signup screens, needs somes fixes including adding hide password icon, fixing shadow in navigation bar at the top, turn nav bar text color to black, change shade of purple on button to more vibrant

---
+++ app/src/main/java/com/example/rentgo/LoginFrag.kt
@@ -0,0 +1,38 @@
+package com.example.rentgo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+import com.example.rentgo.databinding.FragmentLoginBinding
+
+
+class LoginFrag : Fragment() {
+
+
+    lateinit var binding:FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<View>(R.id.textView12).setOnClickListener{
+            view?.findNavController()?.navigate(R.id.action_loginFrag_to_signupFrag)
+        }
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/rentgo/SignupFrag.kt
@@ -0,0 +1,39 @@
+package com.example.rentgo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+import com.example.rentgo.databinding.FragmentLoginBinding
+import com.example.rentgo.databinding.FragmentSignupBinding
+
+
+class SignupFrag : Fragment() {
+
+
+    lateinit var binding: FragmentSignupBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentSignupBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<View>(R.id.textView12).setOnClickListener{
+            view?.findNavController()?.navigate(R.id.action_signupFrag_to_loginFrag)
+        }
+
+
+
+    }
+
+}
